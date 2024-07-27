c72b7bd5e5e4cabb4deed2d123869bcca7552444
tomgllll <139350642+tomgllll@users.noreply.github.com>
2024-07-27 05:00:20 +0300

    review update

--- src/components/specific/contacts-form.ts
+++ src/components/specific/contacts-form.ts
@@ -1,10 +1,24 @@
 import { Form } from '../base/form';
 import { IBuyerInfo } from '../../types';
 import { IEvents } from '../base/events';
+import { ensureElement } from '../../utils/utils';
 
 export class ContactsForm extends Form<IBuyerInfo> {
+	private emailElement: HTMLInputElement;
+	private phoneElement: HTMLInputElement;
+
 
 	constructor(container: HTMLFormElement, events: IEvents) {
 		super(container, events);
+	}
+
+	set email(value: string) {
+		this.emailElement = this.container.elements.namedItem('email') as HTMLInputElement;
+		this.emailElement.value = value;
+	}
+
+	set phone(value: string) {
+		this.phoneElement = this.container.elements.namedItem('phone') as HTMLInputElement;
+		this.phoneElement.value = value;
 	}
 }
